2db8df29b1ec1784400624f0e6d1fe0ef591cb19
husbantech12 <husbantech08@gmail.com>
2026-01-17 18:31:58 +0500

    Remove Frontend

--- frontend/lib/better-auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from 'better-auth/client';
-
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'http://localhost:8000', // Backend URL
-  fetch: globalThis.fetch,
-});
-
-export const { signIn, signUp, signOut, getSession } = authClient;
